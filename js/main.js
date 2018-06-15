8741231ad81a547a0119793cb8ae01a403bc1155
Jose Guerrero <joseguerreroroman@yahoo.com.mx>
2018-06-15 08:10:24 -0500

    Cambios en estilos e imágenes. Cambio en datatables

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 
     revision();
-    // getData();
 
     let data;
 
@@ -14,7 +13,16 @@ $(document).ready(function () {
         columns: [
             { data: 'name' },
             { data: 'rating' },
-            { data: 'contact.site', "orderable": false },
+            { data: 'contact.site', 
+                    "orderable": false,
+                    "render":   function(data, type, row, meta){
+                        if(type === 'display'){
+                            data = '<a href="' + data + '">' + data + '</a>';
+                        }
+            
+                        return data;
+                    }  
+                },
             { data: 'contact.email', "orderable": false  },
             { data: 'contact.phone', "orderable": false  },
             { data: 'address.street', "orderable": false  },
@@ -24,109 +32,8 @@ $(document).ready(function () {
     } 
 );
  }
-    
-    function getData() {
-    
-    $.getJSON('https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json', function (data) {
 
-        let color = data[0].address.location;
-        
-        console.log("este es el dao json", JSON.stringify(color));
-         
-
-
-
-                $('#restaurantsTable').DataTable( {
-                    ajax: {
-                        url: 'https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json',
-                        dataSrc: data
-                    },
-
-                    data: data,
-                    columns: [
-                        { data: 'id' },
-                        { data: 'name' },
-                        { data: 'rating'},
-                        { data: 'address.city'}
-                    ]
-                } 
-            );
-        }
-    )
-    };
-
-
-
-
-
-
-    // $('#get-data').click(function () {
-        
-    //   var showData = $('#tablewrapper');
-
-    //   let content;
-
-    //   $.getJSON('https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json', function (data) {
-    //     console.log(data);
-  
-    //     var items = data.map(function (item) {
-          
-    //       content = 
-    //               "<tr>" +
-    //                     "<td>" + item.id + "</td>" +
-    //                     "<td>"+ item. name + "</td>" +
-    //                     "<td>" +
-    //                         "<table>" +
-    //                             "<thead>" +
-    //                                 "<tr>" +
-    //                                     "<th>Website</th>" +
-    //                                     "<th>Email</th>" +
-    //                                     "<th>Phone</th>" +
-    //                                 "</tr>" +
-    //                             "</thead>" +
-    //                             "<tbody>" +
-    //                                 "<tr>" +
-    //                                     "<td>"+ item.contact.site +"</td>" +
-    //                                     "<td>" + item.contact.email + "</td>" +
-    //                                     "<td>" + item.contact.phone  + "</td>" +
-    //                                 "</tr>" +
-    //                             "</tbody>" +
-    //                         "</table>" +
-    //                     "</td>" +
-    //                     "<td>" +
-    //                         "<table>" +
-    //                             "<thead>" +
-    //                                 "<tr>" +
-    //                                     "<th>Street</th>" +
-    //                                     "<th>City</th>" +
-    //                                     "<th>State</th>" +
-    //                                     "<th>Location</th>" +
-    //                                 "</tr>" +
-    //                             "</thead>" +
-    //                             "<tbody>" +
-    //                                 "<tr>" +
-    //                                     "<td>" + item.address.street + "</td>" +
-    //                                     "<td>" + item.address.city + "</td>" +
-    //                                     "<td>" + item.address.state + "</td>" +
-    //                                     "<td>" + item.address.location + "</td>" +
-    //                                 "</tr>" +
-    //                             "</tbody>" +
-    //                         "</table>" +
-    //                     "</td>" +
-    //                     "<td>"+ item.rating + "<i class=\"far fa-star\"></i></td>" +
-    //                 "</tr>";
-          
-    //       showData.append(content);
-
-    //       return;
-    //     });
-        
-    //     console.log("Datos en el array: ", items);
-
-    //   });
-  
-    // });
-  });
+});
 
 
 
